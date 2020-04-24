3348a465a0de56c457a8e93f9d8f25c4fcbcd7a9
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2020-04-25 00:28:28 +0530

    ackermann function

--- ackermann_function.py
+++ ackermann_function.py
@@ -4,3 +4,13 @@ def ack(m, n):
     if m == 0: return n + 1
     elif n == 0: return ack(m - 1, 1)
     else: return ack(m - 1, ack(m, n - 1))
+
+if __name__ == "__main__":
+    # Get cli input
+    import sys
+    if len(sys.argv) < 3:
+        print("Pass 2 cli numbers")
+    else:
+        m = int(sys.argv[1])
+        n = int(sys.argv[2])
+        print(ack(m, n))
